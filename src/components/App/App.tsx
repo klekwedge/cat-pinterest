ba19a272522e6659e4e53c1207c89bf33c5d8c5c
Andrey <antonov.andrey.10101@gmail.com>
2023-08-15 14:33:30 +0400

    feat: add styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,35 @@
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <header className="header">
+        <div className="header__container _container">
+          <nav className="header__menu menu">
+            <ul className="menu__list">
+              <li className="menu__item">
+                <button type="button" className="menu__button _active">
+                  Все котики
+                </button>
+              </li>
+              <li className="menu__item">
+                <button type="button" className="menu__button" data-favourite>
+                  Любимые котики
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main className="page">
+        <section className="page__cats cats">
+          <div className="cats__container _container">
+            <div className="testOPA" />
+            <ul className="cats__list" />
+            <ul className="cats__list_favourite" />
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
