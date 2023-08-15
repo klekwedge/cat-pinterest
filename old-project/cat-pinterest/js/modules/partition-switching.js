e8b3aac262e0ac44be4f3ee587f724eb2f243348
Andrey <antonov.andrey.10101@gmail.com>
2023-08-15 14:12:58 +0400

    feat: add removal from section of favorite (old)

--- old-project/cat-pinterest/js/modules/partition-switching.js
+++ old-project/cat-pinterest/js/modules/partition-switching.js
@@ -15,9 +15,8 @@ function partitionSwitching() {
         document.querySelector('.cats__list').style.display = 'none';
         catsListFavourite.style.display = 'flex';
 
-        const favouriteCatsArr = JSON.parse(localStorage.getItem('favouriteCats'));
+        let favouriteCatsArr = JSON.parse(localStorage.getItem('favouriteCats'));
         favouriteCatsArr.forEach((favouriteCat) => {
-        //   console.log(favouriteCat);
           const catItem = document.createElement('li');
           catItem.classList.add('cats__item');
 
@@ -31,10 +30,15 @@ function partitionSwitching() {
           catLikeIcon.classList.add('cats__like');
           catLikeIcon.innerHTML = '<svg class="cats__svg" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"> <path class="cats__path" d="M33 6C29.52 6 26.18 7.62 24 10.18C21.82 7.62 18.48 6 15 6C8.84 6 4 10.84 4 17C4 24.56 10.8 30.72 21.1 40.08L24 42.7L26.9 40.06C37.2 30.72 44 24.56 44 17C44 10.84 39.16 6 33 6ZM24.2 37.1L24 37.3L23.8 37.1C14.28 28.48 8 22.78 8 17C8 13 11 10 15 10C18.08 10 21.08 11.98 22.14 14.72H25.88C26.92 11.98 29.92 10 33 10C37 10 40 13 40 17C40 22.78 33.72 28.48 24.2 37.1Z" fill="#F24E1E"/> </svg>';
 
+          catLikeIcon.addEventListener('click', () => {
+            favouriteCatsArr = favouriteCatsArr.filter((cat) => cat.id !== favouriteCat.id);
+            localStorage.setItem('favouriteCats', JSON.stringify(favouriteCatsArr));
+            catsListFavourite.removeChild(catItem);
+          });
+
           catItem.append(catImage);
           catItem.append(catLikeIcon);
-          document.querySelector('.cats__list_favourite').append(catItem);
-          document.querySelector('.cats__list_favourite').append();
+          catsListFavourite.append(catItem);
         });
       } else {
         document.querySelector('.cats__list').style.display = 'flex';
