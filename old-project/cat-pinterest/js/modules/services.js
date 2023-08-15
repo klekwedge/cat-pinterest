c744be8b72c0d6879a683e739353b2307b16c1f2
Andrey <antonov.andrey.10101@gmail.com>
2023-08-15 14:15:41 +0400

    feat: change state of icon when clicked (old)

--- old-project/cat-pinterest/js/modules/services.js
+++ old-project/cat-pinterest/js/modules/services.js
@@ -14,17 +14,20 @@ function getCatsImage() {
     const catLikeIcon = document.createElement('div');
 
     catLikeIcon.classList.add('cats__like');
-    catLikeIcon.innerHTML = '<svg class="cats__svg" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"> <path class="cats__path" d="M33 6C29.52 6 26.18 7.62 24 10.18C21.82 7.62 18.48 6 15 6C8.84 6 4 10.84 4 17C4 24.56 10.8 30.72 21.1 40.08L24 42.7L26.9 40.06C37.2 30.72 44 24.56 44 17C44 10.84 39.16 6 33 6ZM24.2 37.1L24 37.3L23.8 37.1C14.28 28.48 8 22.78 8 17C8 13 11 10 15 10C18.08 10 21.08 11.98 22.14 14.72H25.88C26.92 11.98 29.92 10 33 10C37 10 40 13 40 17C40 22.78 33.72 28.48 24.2 37.1Z" fill="#F24E1E"/> </svg>';
+
+    catLikeIcon.innerHTML = '<svg width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M29 0C25.52 0 22.18 1.62 20 4.18C17.82 1.62 14.48 0 11 0C4.84 0 0 4.84 0 11C0 18.56 6.8 24.72 17.1 34.08L20 36.7L22.9 34.06C33.2 24.72 40 18.56 40 11C40 4.84 35.16 0 29 0ZM20.2 31.1L20 31.3L19.8 31.1C10.28 22.48 4 16.78 4 11C4 7 7 4 11 4C14.08 4 17.08 5.98 18.14 8.72H21.88C22.92 5.98 25.92 4 29 4C33 4 36 7 36 11C36 16.78 29.72 22.48 20.2 31.1Z" fill="#F24E1E"/></svg>';
 
     catLikeIcon.addEventListener('click', () => {
       if (catItem.classList.contains('favourite')) {
         favouriteCatsArr = favouriteCatsArr.filter((cat) => cat.id !== newCat.id);
         localStorage.setItem('favouriteCats', JSON.stringify(favouriteCatsArr));
         catItem.classList.remove('favourite');
+        catLikeIcon.innerHTML = '<svg width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M29 0C25.52 0 22.18 1.62 20 4.18C17.82 1.62 14.48 0 11 0C4.84 0 0 4.84 0 11C0 18.56 6.8 24.72 17.1 34.08L20 36.7L22.9 34.06C33.2 24.72 40 18.56 40 11C40 4.84 35.16 0 29 0ZM20.2 31.1L20 31.3L19.8 31.1C10.28 22.48 4 16.78 4 11C4 7 7 4 11 4C14.08 4 17.08 5.98 18.14 8.72H21.88C22.92 5.98 25.92 4 29 4C33 4 36 7 36 11C36 16.78 29.72 22.48 20.2 31.1Z" fill="#F24E1E"/></svg>';
       } else {
         favouriteCatsArr.push(newCat);
         localStorage.setItem('favouriteCats', JSON.stringify(favouriteCatsArr));
         catItem.classList.add('favourite');
+        catLikeIcon.innerHTML = '<svg width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20 36.7L17.1 34.06C6.8 24.72 0 18.56 0 11C0 4.84 4.84 0 11 0C14.48 0 17.82 1.62 20 4.18C22.18 1.62 25.52 0 29 0C35.16 0 40 4.84 40 11C40 18.56 33.2 24.72 22.9 34.08L20 36.7Z" fill="#F24E1E"/></svg>';
       }
     });
 
