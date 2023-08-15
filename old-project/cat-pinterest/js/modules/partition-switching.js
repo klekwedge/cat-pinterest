5c5ca3b36c55811cdb40675e5147de305e8c0419
Andrey <antonov.andrey.10101@gmail.com>
2023-08-15 14:19:42 +0400

    refactor: improve readability (old)

--- old-project/cat-pinterest/js/modules/partition-switching.js
+++ old-project/cat-pinterest/js/modules/partition-switching.js
@@ -8,12 +8,12 @@ function partitionSwitching() {
 
       menuButton.classList.add('_active');
 
-      const catsListFavourite = document.querySelector('.cats__list_favourite');
+      const catFavouriteList = document.querySelector('.cats__list_favourite');
 
       if (menuButton.hasAttribute('data-favourite')) {
-        catsListFavourite.innerHTML = '';
+        catFavouriteList.innerHTML = '';
         document.querySelector('.cats__list').style.display = 'none';
-        catsListFavourite.style.display = 'flex';
+        catFavouriteList.style.display = 'flex';
 
         let favouriteCatsArr = JSON.parse(localStorage.getItem('favouriteCats'));
 
@@ -28,36 +28,37 @@ function partitionSwitching() {
             catImage.src = favouriteCat.url;
             catImage.alt = 'Cat image';
 
-            const catLikeIcon = document.createElement('div');
-
+            const catLikeIcon = document.createElement('img');
+            catLikeIcon.src = 'cat-pinterest/../assets/png/filled-heart.png';
+            catLikeIcon.alt = 'Heart icon';
             catLikeIcon.classList.add('cats__like');
-            catLikeIcon.innerHTML = '<svg width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20 36.7L17.1 34.06C6.8 24.72 0 18.56 0 11C0 4.84 4.84 0 11 0C14.48 0 17.82 1.62 20 4.18C22.18 1.62 25.52 0 29 0C35.16 0 40 4.84 40 11C40 18.56 33.2 24.72 22.9 34.08L20 36.7Z" fill="#F24E1E"/></svg>';
 
             catLikeIcon.addEventListener('click', () => {
               const featuredItemsHomepage = document.querySelectorAll('.favourite');
 
               featuredItemsHomepage.forEach((featuredItemHomepage) => {
-                const divWrapper = featuredItemHomepage.lastChild;
+                const mainPageImage = featuredItemHomepage.lastChild;
+
                 if (featuredItemHomepage.getAttribute('id') === favouriteCat.id) {
                   featuredItemHomepage.classList.remove('favourite');
-                  divWrapper.innerHTML = '<svg width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M29 0C25.52 0 22.18 1.62 20 4.18C17.82 1.62 14.48 0 11 0C4.84 0 0 4.84 0 11C0 18.56 6.8 24.72 17.1 34.08L20 36.7L22.9 34.06C33.2 24.72 40 18.56 40 11C40 4.84 35.16 0 29 0ZM20.2 31.1L20 31.3L19.8 31.1C10.28 22.48 4 16.78 4 11C4 7 7 4 11 4C14.08 4 17.08 5.98 18.14 8.72H21.88C22.92 5.98 25.92 4 29 4C33 4 36 7 36 11C36 16.78 29.72 22.48 20.2 31.1Z" fill="#F24E1E"/></svg>';
+                  mainPageImage.src = 'cat-pinterest/../assets/png/hollow-heart.png';
                 }
               });
 
               favouriteCatsArr = favouriteCatsArr.filter((cat) => cat.id !== favouriteCat.id);
 
               localStorage.setItem('favouriteCats', JSON.stringify(favouriteCatsArr));
-              catsListFavourite.removeChild(catItem);
+              catFavouriteList.removeChild(catItem);
             });
 
             catItem.append(catImage);
             catItem.append(catLikeIcon);
-            catsListFavourite.append(catItem);
+            catFavouriteList.append(catItem);
           });
         }
       } else {
         document.querySelector('.cats__list').style.display = 'flex';
-        catsListFavourite.style.display = 'none';
+        catFavouriteList.style.display = 'none';
       }
     });
   });
--- old-project/cat-pinterest/js/modules/services.js
+++ old-project/cat-pinterest/js/modules/services.js
@@ -1,5 +1,5 @@
 function getCatsImage() {
-  function createCatImage(newCat) {
+  function createCatItem(newCat) {
     const catList = document.querySelector('.cats__list');
 
     const catItem = document.createElement('li');
@@ -12,27 +12,23 @@ function getCatsImage() {
     catImage.alt = 'Cat image';
 
     const catLikeIcon = document.createElement('img');
-
-    catLikeIcon.src = 'cat-pinterest/../assets/png/like.png';
+    catLikeIcon.classList.add('cats__like');
+    catLikeIcon.src = 'cat-pinterest/../assets/png/hollow-heart.png';
     catLikeIcon.alt = 'Heart icon';
 
     catLikeIcon.addEventListener('mouseover', () => {
-      catLikeIcon.src = 'cat-pinterest/../assets/png/like-2.png';
+      catLikeIcon.src = 'cat-pinterest/../assets/png/filled-heart.png';
     });
 
     function deleteHoverEffect() {
-      catLikeIcon.src = 'cat-pinterest/../assets/png/like.png';
+      catLikeIcon.src = 'cat-pinterest/../assets/png/hollow-heart.png';
     }
+
     catLikeIcon.addEventListener('mouseout', deleteHoverEffect);
-
-    // list.append(image);
-
-    catLikeIcon.classList.add('cats__like');
-
-    // catLikeIcon.innerHTML = '<svg class="cats__svg" width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="cats__path" d="M29 0C25.52 0 22.18 1.62 20 4.18C17.82 1.62 14.48 0 11 0C4.84 0 0 4.84 0 11C0 18.56 6.8 24.72 17.1 34.08L20 36.7L22.9 34.06C33.2 24.72 40 18.56 40 11C40 4.84 35.16 0 29 0ZM20.2 31.1L20 31.3L19.8 31.1C10.28 22.48 4 16.78 4 11C4 7 7 4 11 4C14.08 4 17.08 5.98 18.14 8.72H21.88C22.92 5.98 25.92 4 29 4C33 4 36 7 36 11C36 16.78 29.72 22.48 20.2 31.1Z" fill="#F24E1E"/></svg>';
 
     catLikeIcon.addEventListener('click', () => {
       let favouriteCatsArr;
+
       if (JSON.parse(localStorage.getItem('favouriteCats'))) {
         favouriteCatsArr = JSON.parse(localStorage.getItem('favouriteCats'));
       } else {
@@ -40,39 +36,23 @@ function getCatsImage() {
       }
 
       if (catItem.classList.contains('favourite')) {
+        catLikeIcon.src = 'cat-pinterest/../assets/png/hollow-heart.png';
         catLikeIcon.addEventListener('mouseout', deleteHoverEffect);
+
         favouriteCatsArr = favouriteCatsArr.filter((cat) => cat.id !== newCat.id);
         localStorage.setItem('favouriteCats', JSON.stringify(favouriteCatsArr));
+
         catItem.classList.remove('favourite');
-        catLikeIcon.innerHTML = '<svg class="cats__svg" width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="cats__path" d="M29 0C25.52 0 22.18 1.62 20 4.18C17.82 1.62 14.48 0 11 0C4.84 0 0 4.84 0 11C0 18.56 6.8 24.72 17.1 34.08L20 36.7L22.9 34.06C33.2 24.72 40 18.56 40 11C40 4.84 35.16 0 29 0ZM20.2 31.1L20 31.3L19.8 31.1C10.28 22.48 4 16.78 4 11C4 7 7 4 11 4C14.08 4 17.08 5.98 18.14 8.72H21.88C22.92 5.98 25.92 4 29 4C33 4 36 7 36 11C36 16.78 29.72 22.48 20.2 31.1Z" fill="#F24E1E"/></svg>';
       } else {
+        catLikeIcon.src = 'cat-pinterest/../assets/png/filled-heart.png';
         catLikeIcon.removeEventListener('mouseout', deleteHoverEffect);
+
         favouriteCatsArr.push(newCat);
         localStorage.setItem('favouriteCats', JSON.stringify(favouriteCatsArr));
+
         catItem.classList.add('favourite');
-        catLikeIcon.innerHTML = '<svg class="cats__svg" width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="cats__path" d="M20 36.7L17.1 34.06C6.8 24.72 0 18.56 0 11C0 4.84 4.84 0 11 0C14.48 0 17.82 1.62 20 4.18C22.18 1.62 25.52 0 29 0C35.16 0 40 4.84 40 11C40 18.56 33.2 24.72 22.9 34.08L20 36.7Z" fill="#F24E1E"/></svg>';
       }
     });
-
-    // catItem.addEventListener('mouseover', (event) => {
-    //   console.log(event.target.getAttribute('class'));
-    //   if (
-    //     event.target.getAttribute('class') === 'cats__svg'
-    //     || event.target.getAttribute('class') === 'cats__path'
-    //   ) {
-    //     catLikeIcon.innerHTML = '<svg class="cats__svg" width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg"> <path class="cats__path" d="M20 36.7L17.1 34.06C6.8 24.72 0 18.56 0 11C0 4.84 4.84 0 11 0C14.48 0 17.82 1.62 20 4.18C22.18 1.62 25.52 0 29 0C35.16 0 40 4.84 40 11C40 18.56 33.2 24.72 22.9 34.08L20 36.7Z" fill="#F24E1E"/></svg>';
-    //   }
-    // });
-
-    // catItem.addEventListener('mouseout', (event) => {
-    //   console.log(event.target.getAttribute('LOL'));
-    //   if (
-    //     event.relatedTarget.getAttribute('class') !== 'cats__svg'
-    //     || event.target.getAttribute('class') !== 'cats__path'
-    //   ) {
-    //     catLikeIcon.innerHTML = '<svg class="cats__svg" width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="cats__path" d="M20 36.7L17.1 34.06C6.8 24.72 0 18.56 0 11C0 4.84 4.84 0 11 0C14.48 0 17.82 1.62 20 4.18C22.18 1.62 25.52 0 29 0C35.16 0 40 4.84 40 11C40 18.56 33.2 24.72 22.9 34.08L20 36.7Z" fill="#F24E1E"/></svg>';
-    //   }
-    // });
 
     catItem.append(catImage);
     catItem.append(catLikeIcon);
@@ -81,7 +61,7 @@ function getCatsImage() {
 
   function showData(data) {
     for (let i = 0; i < data.length; i += 1) {
-      createCatImage(data[i]);
+      createCatItem(data[i]);
     }
   }
 
