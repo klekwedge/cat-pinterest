e9b46a431977d561bf5ac792ae8a9a0af4f2bd84
Andrey <antonov.andrey.10101@gmail.com>
2023-08-16 08:34:22 +0400

    feat: add removal of cats on the favorites page

--- src/pages/FavouritePage/FavouritePage.tsx
+++ src/pages/FavouritePage/FavouritePage.tsx
@@ -1,8 +1,15 @@
 import { Button, Card, Flex, Image } from '@mantine/core';
-import { useAppSelector } from '../../hooks/useRedux';
+import { useAppDispatch, useAppSelector } from '../../hooks/useRedux';
+import { removeCatFromFavourite } from '../../slices/catsSlice/catsSlice';
+import ICat from '../../types';
 
 function FavouritePage() {
   const { favouriteCats } = useAppSelector((state) => state.cats);
+  const dispatch = useAppDispatch();
+
+  const removeFromFavourite = (cat: ICat) => {
+    dispatch(removeCatFromFavourite(cat));
+  };
 
   return (
     <main className="page">
@@ -14,8 +21,16 @@ function FavouritePage() {
                 <Card.Section>
                   <Image src={cat.url} alt="cat" width="100%" height="160px" fit="cover" />
                 </Card.Section>
-                <Button display="block" ml="auto" variant="light" color="blue" mt="md" radius="md">
-                  Like
+                <Button
+                  display="block"
+                  ml="auto"
+                  variant="light"
+                  color="blue"
+                  mt="md"
+                  radius="md"
+                  onClick={() => removeFromFavourite(cat)}
+                >
+                  Unlike
                 </Button>
               </Card>
             ))}
